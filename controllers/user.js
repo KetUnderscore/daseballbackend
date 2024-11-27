828a675f2ff8dba3cb71884cba7c0c30e98ac934
KetUnderscore <tannersock14@gmail.com>
2024-11-26 23:10:37 -0800

    BLEGH

---
+++ controllers/user.js
@@ -0,0 +1,75 @@
+const User = require('../models/SiteUser')
+
+// User Related Routes here ----------------------------------------------------------------------------------------
+exports.signup = async(req, res, next) => {
+
+    const {email} = req.body;
+    const userExists = await User.findOne({email})
+
+    if (userExists) {
+        return res.status(400).json({
+            sucess: false,
+            message: "Email already exists."
+        })
+    }
+
+    try {
+        const user = await User.create(req.body)
+        res.status(201).json({
+            sucess: true,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            sucess: false,
+            message: error.message
+        })
+    }
+}
+
+exports.signin = async(req, res, next) => {
+
+    try {
+        const {username, password} = req.body
+        if (!username || !password) {
+            return res.status(400).json({
+                sucess: false,
+                message: 'Username and password are required.'
+            })
+        }
+
+        // Check if user is in database
+        const user = await User.findOne({username})
+
+        if (!user) {
+            return res.status(400).json({
+                sucess: false,
+                message: "Invalid credentials."
+            })
+        }
+
+        // Check password
+        if (user.password != password) {
+            return res.status(400).json({
+                sucess: false,
+                message: "Password is incorrect."
+            })
+        }
+
+        const token = await user.jwtGenerateToken()
+
+        res.status(200).json({
+            sucess: true,
+            user
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({
+            sucess: false,
+            message: "Cannot login, check your email and password."
+        })
+    }
+
+}
